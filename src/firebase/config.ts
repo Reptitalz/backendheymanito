983fa496cf4fd782f390040176ea218f98fad13a
Pablo Esteban Pedroza Bernal <reptitalz@gmail.com>
2025-10-30 22:07:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1128284178-7d125",
+  "appId": "1:722319793837:web:b16e7d9787d8082bab3024",
+  "apiKey": "AIzaSyCUpVguIRuwkRaaY1KyGMXYaq-BgLzpPm4",
+  "authDomain": "studio-1128284178-7d125.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "722319793837"
+};
