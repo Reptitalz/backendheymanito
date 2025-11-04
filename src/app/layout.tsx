8fdcd54eb4ec0143a27369e1d201cd6583d2c511
Pablo Esteban Pedroza Bernal <reptitalz@gmail.com>
2025-11-04 21:31:44 +0000

    quiero que este icono se encuentre en el icono de la pagina , en las pes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Poppins:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
